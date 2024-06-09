de31b2825942ca6e2926dec0066168c5fe2c9c81
Luke Kerbs <luke@Lukes-MBP-3.lan>
2024-06-09 13:44:14 -0700

    improved readability

--- easy_apply.py
+++ easy_apply.py
@@ -41,93 +41,6 @@ def check_sign_in_page():
     xpath_expression = 'a[class="sign-in-form__sign-in-cta btn-md btn-secondary babybear:w-full block mb-3 mx-auto min-h-[40px] py-1"]'
     sign_in_btn = get_element(by=By.CLASS_NAME, selector=xpath_expression, timeout=5)
     return sign_in_btn
-
-    
-
-
-
-# Open the website
-# url = 'https://www.linkedin.com'
-# print(f'\nOpening: {url}\n')
-# driver.get(url)
-
-
-
-# - - LOG IN PAGE - -
-# Enter username
-version_2_btn = check_sign_in_page()
-if not version_2_btn:
-    username_field = get_element(by=By.CSS_SELECTOR, selector='input[autocomplete="username"]', timeout=3)
-    username_field.send_keys(USERNAME)
-    time.sleep(1)
-
-    password_field = get_element(by=By.ID, selector='session_password', timeout=3)
-    password_field.send_keys(PASSWORD)
-    time.sleep(1)
-
-    # Submit sign-in credentials
-    sign_in_btn = driver.find_element(By.CSS_SELECTOR, 'button[data-id="sign-in-form__submit-btn"]')
-    sign_in_btn.click()
-else:
-    version_2_btn.click()
-    username_field = get_element(by=By.CSS_SELECTOR, selector='input[id="username"]', timeout=3)
-    username_field.send_keys(USERNAME)
-    time.sleep(1)
-
-    password_field = get_element(by=By.ID, selector='password', timeout=3)
-    password_field.send_keys(PASSWORD)
-    time.sleep(1)
-
-    # Submit sign-in credentials
-    sign_in_btn = driver.find_element(By.CSS_SELECTOR, 'button[class="btn__primary--large from__button--floating"]')
-    sign_in_btn.click()
-
-# - - SECURITY CHALLENGE - -
-# Wait for security challenge 
-show_popup(message='Complete security challenge if necessary and press CONTINUE.\n\n(If you are already on the LinkedIn homepage, just press CONTINUE): ')
-time.sleep(3)
-
-# - - LINKEDIN HOMEPAGE FEED - -
-# Enter search query in LinkedIn search
-search_field = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Search"]')
-search_field.send_keys(INDUSTRY)
-search_field.send_keys(Keys.RETURN)
-time.sleep(5)
-
-
-#- - PEOPLE RESULTS PAGE - - 
-# Select the 'Jobs' search filter 
-try:
-    # Apply people filter 
-    filter_buttons = driver.find_elements(By.CSS_SELECTOR, '.artdeco-pill.artdeco-pill--slate.artdeco-pill--choice.artdeco-pill--2.search-reusables__filter-pill-button')
-    jobs_button = None
-    for button in filter_buttons:
-        if 'Jobs' in button.text:
-            jobs_button = button
-            break
-    if jobs_button:
-        jobs_button.click()
-    else:
-        print('Could not find jobs button')
-        
-except Exception as e:
-    print(f'ERROR: {e}')
-    
-# Select the 'Easy Apply' filter
-xpath_expression = '//button[@aria-label="Easy Apply filter."]'
-actively_hiring_filter = get_element(by=By.XPATH, selector=xpath_expression, timeout=5)
-if actively_hiring_filter:
-    actively_hiring_filter.click()
-    easy_apply_clicked = True
-else:
-    easy_apply_clicked = False
-
-if not easy_apply_clicked:
-    show_popup(message='Manually select the easy apply filter press CONTINUE: ')
-        
-
-# Manually add any additional job search filters.
-show_popup(message='Manually select and apply any additional job search filters in the browser press CONTINUE:')
     
 def scroll_to_element(driver, element):
     actions = ActionChains(driver)
@@ -331,8 +244,85 @@ def get_nav_pages(driver):
     pages = pagination_container.find_elements(By.CSS_SELECTOR, "li[data-test-pagination-page-btn]")
     return pages
 
-pages = get_nav_pages(driver)
+
+
+# - - - - - - -  M A I N   E X E C U T I O N   S T A R T - - - - - - - - - 
+# - - LOG IN PAGE - -
+# Enter username
+version_2_btn = check_sign_in_page()
+if not version_2_btn:
+    username_field = get_element(by=By.CSS_SELECTOR, selector='input[autocomplete="username"]', timeout=3)
+    username_field.send_keys(USERNAME)
+    time.sleep(1)
+
+    password_field = get_element(by=By.ID, selector='session_password', timeout=3)
+    password_field.send_keys(PASSWORD)
+    time.sleep(1)
+
+    # Submit sign-in credentials
+    sign_in_btn = driver.find_element(By.CSS_SELECTOR, 'button[data-id="sign-in-form__submit-btn"]')
+    sign_in_btn.click()
+else:
+    version_2_btn.click()
+    username_field = get_element(by=By.CSS_SELECTOR, selector='input[id="username"]', timeout=3)
+    username_field.send_keys(USERNAME)
+    time.sleep(1)
+
+    password_field = get_element(by=By.ID, selector='password', timeout=3)
+    password_field.send_keys(PASSWORD)
+    time.sleep(1)
+
+    # Submit sign-in credentials
+    sign_in_btn = driver.find_element(By.CSS_SELECTOR, 'button[class="btn__primary--large from__button--floating"]')
+    sign_in_btn.click()
+
+# - - SECURITY CHALLENGE - -
+# Wait for security challenge 
+show_popup(message='Complete security challenge if necessary and press CONTINUE.\n\n(If you are already on the LinkedIn homepage, just press CONTINUE): ')
+time.sleep(3)
+
+# - - LINKEDIN HOMEPAGE FEED - -
+# Enter search query in LinkedIn search
+search_field = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Search"]')
+search_field.send_keys(INDUSTRY)
+search_field.send_keys(Keys.RETURN)
+time.sleep(5)
+
+
+#- - PEOPLE RESULTS PAGE - - 
+# Select the 'Jobs' search filter 
+try:
+    # Apply people filter 
+    filter_buttons = driver.find_elements(By.CSS_SELECTOR, '.artdeco-pill.artdeco-pill--slate.artdeco-pill--choice.artdeco-pill--2.search-reusables__filter-pill-button')
+    jobs_button = None
+    for button in filter_buttons:
+        if 'Jobs' in button.text:
+            jobs_button = button
+            break
+    if jobs_button:
+        jobs_button.click()
+    else:
+        print('Could not find jobs button')
+        
+except Exception as e:
+    print(f'ERROR: {e}')
     
+# Select the 'Easy Apply' filter
+xpath_expression = '//button[@aria-label="Easy Apply filter."]'
+actively_hiring_filter = get_element(by=By.XPATH, selector=xpath_expression, timeout=5)
+if actively_hiring_filter:
+    actively_hiring_filter.click()
+    easy_apply_clicked = True
+else:
+    easy_apply_clicked = False
+
+if not easy_apply_clicked:
+    show_popup(message='Manually select the easy apply filter press CONTINUE: ')
+        
+# Manually add any additional job search filters.
+show_popup(message='Manually select and apply any additional job search filters in the browser press CONTINUE:')
+
+pages = get_nav_pages(driver)
 try:
     num_pages = len(pages)
     for i in range(num_pages):
@@ -355,14 +345,6 @@ try:
                 continue
             easy_apply_button.click()
             time.sleep(4)
-
-            # try:
-            #     xpath_expression = '//button[@class="jobs-apply-button artdeco-button artdeco-button--3 artdeco-button--primary ember-view"]'
-            #     easy_apply_button = driver.find_element(By.XPATH, xpath_expression)
-            #     easy_apply_button.click()
-            #     time.sleep(4)
-            # except:
-            #     continue
 
             easy_apply(driver)
 
